9c3a0aa370524b4fe494070fcfc82060a7e6fae6
Max Girkens <maxgirkens@gmail.com>
2022-11-10 08:10:19 +0100

    cleanup

--- bjj-aachen/header.php
+++ bjj-aachen/header.php
@@ -10,12 +10,14 @@
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-0">
             <div class="container-fluid">
                 <div class="bb"></div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#nav-primary"
+                >
                     <span class="navbar-toggler-icon text-white"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="nav-primary">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <?php foreach (bjjac_get_menu_items('primary') as $menuItem): ?>
                             <li class="nav-item">
