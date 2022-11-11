1aa49752bf7afa357bad646bb5c3774a713f8724
Max Girkens <maxgirkens@gmail.com>
2022-11-11 13:35:12 +0100

    title

--- bjj-aachen/head.php
+++ bjj-aachen/head.php
@@ -2,9 +2,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <title><?php bloginfo('name'); ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico">
     <?php wp_head(); ?>
 </head>
